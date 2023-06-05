5559e6cff68410aaedc069c7a9ea69db1f420938
Cristina <cristinaforescampos1992@gmail.com>
2023-06-05 19:34:58 +0200

    Connection data base

---
+++ backend/src/dataBase/index.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "../loadEnviroment.js";
+
+export const connectDb = async (mongoUrl: string) => {
+  await mongoose.connect(mongoUrl);
+
+  mongoose.set("toJSON", {
+    virtuals: true,
+    transform(doc, ret) {
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    },
+  });
+};
--- backend/src/server/loadEnviroment.ts
+++ backend/src/server/loadEnviroment.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const environment = {
   port: process.env.PORT,
+  mongoDbUrl: process.env.MONGODB_URL,
 };
 
 export default environment;
