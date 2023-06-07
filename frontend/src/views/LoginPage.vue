240d961bd52e00d6c86ed2e529efbdd36f46472d
Cristina <cristinaforescampos1992@gmail.com>
2023-06-07 14:29:45 +0200

    Views LoginPage

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header></Header>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Header from "../components/Header/Header.vue";
+import LoginForm from "../components/LoginForm/LoginForm.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Header,
+
+    LoginForm,
+  },
+});
+</script>
